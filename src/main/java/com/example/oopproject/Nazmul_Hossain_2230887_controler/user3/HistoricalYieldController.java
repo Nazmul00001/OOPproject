3a07c231f07dc54a30eaf1c8f241d2592621f55d
Nazmul00001 <fair6149@gmail.com>
2024-12-10 11:24:57 +0600

    User4 scene update and fixing some user3 scenes

---
+++ src/main/java/com/example/oopproject/Nazmul_Hossain_2230887_controler/user3/HistoricalYieldController.java
@@ -0,0 +1,35 @@
+package com.example.oopproject.Nazmul_Hossain_2230887_controler.user3;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class HistoricalYieldController
+{
+    @javafx.fxml.FXML
+    private TextField equipmentDetails;
+    @javafx.fxml.FXML
+    private ComboBox analysisFactors;
+    @javafx.fxml.FXML
+    private Button submitPlanButton;
+    @javafx.fxml.FXML
+    private Button generateGraphButton;
+    @javafx.fxml.FXML
+    private TextField landDetails;
+    @javafx.fxml.FXML
+    private TableView historicalDataTable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitPlan(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateGraph(ActionEvent actionEvent) {
+    }
+}
