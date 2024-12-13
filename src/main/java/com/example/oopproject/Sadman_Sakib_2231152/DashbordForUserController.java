0ddeb74dc3bef70ba137e531040f6556507e4766
sadman160400 <2231152@iub.edu.bd>
2024-12-14 04:35:33 +0600

    update module-info.java for user 1 and user 2 dashbord

--- src/main/java/com/example/oopproject/Sadman_Sakib_2231152/DashbordForUserController.java
+++ src/main/java/com/example/oopproject/Sadman_Sakib_2231152/DashbordForUserController.java
@@ -1,4 +1,14 @@
 package com.example.oopproject.Sadman_Sakib_2231152;
 
+import javafx.event.Event;
+import javafx.scene.control.ComboBox;
+
 public class DashbordForUserController {
+
+    @javafx.fxml.FXML
+    private ComboBox Combobox;
+
+    @javafx.fxml.FXML
+    public void view(Event event) {
+    }
 }
